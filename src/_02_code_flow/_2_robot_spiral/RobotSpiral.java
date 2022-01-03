31cb649cab4a97729d86aab1ab5622edc4233d71
InfiniteKon <97066861+InfiniteKon@users.noreply.github.com>
2022-01-03 15:16:25 -0800

    Update RobotSpiral.java

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,16 +12,22 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
-		
+		Robot e = new Robot();
 		// Set your robot's pen down 
-		
+		e.penDown();
 		// SPEED. Set the robot to go at max speed (100)
-
+e.setSpeed(100);
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
 			//        Start its value as zero.
-
+int count = 0;
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
+while (count < 50 ) {
+	e.setRandomPenColor();
+	e.move(5*count);
+	e.turn(360/7);
+	count+=1;
+	e.setPenWidth(count);
+}
 			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
 
 			// DRAW.    Move the robot (5*count) pixels
